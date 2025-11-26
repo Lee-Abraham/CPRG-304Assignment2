ba5c0652b186911ec2a318fcd715622b4eda6d66
launchland <benjulian.noel@gmail.com>
2025-11-25 22:14:41 -0700

    Missing files rom master

---
+++ src/implementations/MyDLLNode.java
@@ -0,0 +1,14 @@
+package implementations;
+
+class MyDLLNode<E> {
+
+    E data;
+    MyDLLNode<E> next;
+    MyDLLNode<E> prev;
+
+    public MyDLLNode(E data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
